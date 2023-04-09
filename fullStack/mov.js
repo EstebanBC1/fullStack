013be1275e01e8308f94345bf20a4f6faeac873c
EstebanBC1 <Estebanbcastro@gmail.com>
2023-04-09 00:10:35 -0700

    Changes to main index file and addting top bar feature

--- fullStack/mov.js
+++
@@ -1,7 +0,0 @@
-let userMessage = prompt("Is this what you really want?");
-userMessage = userMessage.toLowerCase(); 
-if (userMessage === "yes"){
-    console.log("this was a valid entry!");
-}else{
-    console.log("this was an invalid entry")
-}
